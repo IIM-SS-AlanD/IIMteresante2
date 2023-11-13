27d0e43341a15bf1e50fe6dfdf3c20ce4adaff8a
IIM-SS-AlanD <divulgacion.iim.20@gmail.com>
2023-11-12 19:04:24 -0600

    Primer push

---
+++ iimteresante/scripts/menu.js
@@ -0,0 +1,37 @@
+const menuHeaderGenerator = 
+    <nav class="navigation-desktop">
+        <div class="logo unamL">
+            <a href="https://www.unam.mx/"><img src="/images/unam_logo_blanco-02.svg" alt="Logo UNAM"></a>
+        </div>
+        <div class="logo iimL">
+            <a href="https://www.iim.unam.mx/inicio.html"><img src="/images/instituto_logo_blanco_mesa-de-trabajo-1.svg" alt="Logo Instituto"></a>
+        </div>
+        <div class="menu-item">
+            <a href="/index.html">INICIO</a>
+        </div>
+        <div class="menu-item">
+            <a href="/pages/servicios.html">SERVICIOS</a>
+        </div>
+        <div class="menu-item">
+            <a href="/pages/investigacion.html">INVESTIGACI&#211;N</a>
+        </div>
+        <div class="menu-item">
+            <a href="/pages/patentes.html">PATENTES</a>
+        </div>
+        <div class="menu-item">
+            <a href="/pages/convenios.html">CONVENIOS</a>
+        </div>
+        <div class="menu-item">
+            <a href="/pages/innovacion.html">INNOVACI&#211;N</a>
+        </div>
+        <div class="menu-item">
+            <a href="/pages/normatividad.html">NORMATIVIDAD</a>
+        </div>
+        <div class="menu-item">
+            <a href="/pages/gestionCalidad.html">GESTI&#211;N DE LA CALIDAD</a>
+        </div>
+        <div class="menu-toggle-container">
+            <button class="menu-toggle">☰</button>
+        </div>
+    </nav>
+;
